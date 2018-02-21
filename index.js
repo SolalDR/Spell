83ed4775a1a2f488ade73b2a55e5b671f1a00c13
SKARGOVSKII Nikita <nskargovskii@g-a102-02.local>
2018-02-21 12:26:13 +0100

    browserify + babel + annyang

---
+++ index.js
@@ -0,0 +1,23 @@
+import annyang from 'annyang'
+
+if (annyang) {
+    // Let's define a command. 
+    annyang.setLanguage('fr-FR')
+
+    var commands = {
+      "la forêt s'assombrit": function() { alert('Hello world!'); },
+      "la forêt sa sonnerie": function() { alert('Hello world!'); }
+    };
+    
+
+
+    annyang.addCallback('result', function(phrases){
+        console.log(phrases)
+        console.log('ok')
+    })
+    // Add our commands to annyang 
+    annyang.addCommands(commands);
+   
+    // Start listening. 
+    annyang.start();
+  }
