3a0b202f0ed1e69c93c777b8fb7e47d114700038
SolalDR <solal.dussout-revel@hotmail.fr>
2018-03-19 17:59:49 +0100

    Add MaterialIcons & refactor all object with Bard namespace

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-import Book from "./bard/Book.js";
-import Fragment from "./bard/Fragment.js";
-import TextElement from "./bard/elements/Text.js";
-import MeshElement from "./bard/elements/Mesh.js";
-import CharacterElement from "./bard/elements/Character.js";
 import * as THREE from "three"; 
-
+import * as Bard from "./bard/bard.js"
 
 // Custom Fragment
-
-class StartFragment extends Fragment {
+class StartFragment extends Bard.Fragment {
 
 	constructor(){ super(); }
 
@@ -17,15 +11,23 @@ class StartFragment extends Fragment {
 		var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 		this.cube = new THREE.Mesh( geometry, material );
 		
+		this.addSpeechRecognition();
 		
-		this.addElement( new MeshElement({ mesh: this.cube, group: "scene" 	}));
-		this.addElement( new TextElement({ 
-			text: "Un soir alors qu'ils observent le ciel étoilé d'une belle nuit d'été... une étrange comète traverse l'atmosphère ... pour disparaître non loin de là. \"Allons voir cela de plus prêt !\" s'exclament en coeur nos deux héros... Soundain des bruits étranges parviennent de la forêt." ,
+		this.addElement( new Bard.MeshElement({ mesh: this.cube, group: "scene" }));
+		this.addElement( new Bard.TextElement({ 
+			text: [
+				"Un soir alors qu'ils <span data-speech='test_recognition'>observent le ciel étoilé</span> d'une belle nuit d'été", 
+				"une étrange comète traverse l'atmosphère ... pour disparaître non loin de là.", 
+				"\"Allons voir cela de plus prêt !\" s'exclament en coeur nos deux héros...", 
+				"Soundain des bruits étranges parviennent de la forêt."
+			],
 			align: "bottom-left",
 			position: {x: "40px", y:"-20px"},
 			name: "mainText"
 		}));
-		this.addElement( new CharacterElement( {group: "scene", name: "guy"} ) );
+
+
+		this.addElement( new Bard.CharacterElement( {group: "scene", name: "guy"} ) );
 		
 		for(var i=0; i<this.elements.length; i++){
 			this.elements[i].display();
@@ -48,7 +50,7 @@ class StartFragment extends Fragment {
 
 window.addEventListener("load", function(){
 
-    var book = new Book();
+    var book = new Bard.Book();
     var frag = new StartFragment();
 
     book.addFragment(frag);
