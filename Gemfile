03008ec9576fb6f97232e49860a4064c84390dc2
DUSSOUTREVEL Solal <sdussoutrevel@g-a101-24.local>
2018-04-17 15:07:58 +0200

    Add webpack

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+
+# Gemfile
+gem 'webpacker', '~> 3.4'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
---
+++ app/javascript/packs/components/header.js
@@ -0,0 +1,30 @@
+
+export default  {
+
+  dropEvent: function(btn){
+    btn.addEventListener("click", function(e){
+      var drop = this.parentNode.querySelector(".header__drop"); 
+      if( drop.className.match("header__drop--close") ){
+        drop.classList.replace("header__drop--close", "header__drop--open")
+      } else {
+        drop.classList.replace("header__drop--open", "header__drop--close")
+      }
+
+      e.preventDefault();
+    }, false)
+  },
+
+  manageDropBtns: function() {
+    this.dropBtn = this.header.querySelectorAll(".header__drop-btn");
+    for(var i=0; i<this.dropBtn.length; i++){
+      this.dropEvent(this.dropBtn[i]);
+    }
+  },
+
+  init: function(){
+    this.header = document.querySelector(".header");
+    this.manageDropBtns();
+  }
+
+}
+
